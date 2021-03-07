00e4253d4997a752cd95d2d43e59b3a76c8051fb
javirima <71288541+javirima@users.noreply.github.com>
2021-03-07 01:01:09 -0600

    sequencial search update

--- Rust/caso3/src/main.rs
+++ Rust/caso3/src/main.rs
@@ -1,28 +1,49 @@
+#![allow(unused)]
+use std::time::Instant;
+
 
 
 fn main() {
-    println!("Sort numbers ascending");
-    let mut numbers = [4, 65, 2, -31, 0, 99, 2, 83, 782, 1];
-    println!("Before: {:?}", numbers);
-    bubble_sort(&mut numbers);
-    println!("After:  {:?}\n", numbers);
 
+    let start = Instant::now();
+
+    println!("sequencial search");
+    let mut numbers:Vec<i128> = Vec::new();
+    grow_array(&mut numbers);
+    println!("numbers:{:?}",numbers);
+    let elapsed = start.elapsed();
+
+    sequencial_search(&mut numbers,6000);
+    println!("Millis: {} ms", elapsed.as_millis());
+
+    /*
     println!("Sort strings alphabetically");
     let mut strings = ["beach", "hotel", "airplane", "car", "house", "art"];
     println!("Before: {:?}", strings);
     bubble_sort(&mut strings);
     println!("After:  {:?}\n", strings);
+    */
 }
 
 
-
-
-pub fn bubble_sort<T: Ord>(arr: &mut [T]) {
-    for i in 0..arr.len() {
-        for j in 0..arr.len() - 1 - i {
-            if arr[j] > arr[j + 1] {
-                arr.swap(j, j + 1);
-            }
+pub fn grow_array(arr : &mut Vec<i128>){
+    let mut add_number = 5000;
+    loop{
+        if add_number == 0{
+            break;
         }
+        arr.push(add_number);
+        add_number-=1;
     }
+}
+
+pub fn sequencial_search<T: Ord>(arr: &mut [T], elemento_buscar:T) {
+   for index in 0..arr.len() {
+       if arr[index] == elemento_buscar {
+           println!("Encontrado\n");
+           return;
+       }
+   }
+   println!("No se encontro el numero");
+   return;
 }
