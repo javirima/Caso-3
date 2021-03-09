405eb07798ad0e0518e548739f0302755ad51b7b
Daniel-TEC-17 <79490224+Daniel-TEC-17@users.noreply.github.com>
2021-03-09 13:02:10 -0600

    Cambios

--- busquedaLineal.cpp
+++ busquedaLineal.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
 #include <stdlib.h>
+#include <ctime>
 
 using namespace std;
 
-int lineal_search(int *array, int searched, int arraySize)
+const int SIZE = 8192;  /* buffer size for reading /proc/<pid>/status */
+
+
+
+
+void lineal_search(int *array, int searched, int arraySize)
 {
+    double time;
+    double initialTime = clock();
+    bool flag = false;
     for (int i = 0; i< arraySize; i++) {
         if (searched == array[i]) {
             cout << "Se encuentra en la posicion " << i + 1 << endl;
+            flag = true;
+            cout << clock()<<endl;
+            cout << initialTime << endl;
+            time=1000L*(clock()-initialTime)/CLOCKS_PER_SEC;
+            cout << "El tiempo de ejecucion es de " << time << endl;
+            return;
         }
    }
+   if(flag != true){
+       cout << "El numero no se encuentra en el array" << endl;
+   }
+    cout << clock()<<endl;
+    cout << initialTime << endl;
+    time=1000L*(clock()-initialTime)/CLOCKS_PER_SEC;
+    cout << "El tiempo de ejecucion es de " << time << endl;
 }
 
 
@@ -31,17 +53,16 @@ int main()
     int array[arraySize];
 
     for (int i = 0; i < arraySize; i++) {
-        array[i] = rand() % 100;
+        array[i] = rand();
     }
-
+    ///cout << "Tamano del array: " << (array)
     cout << endl;
     cout << "Array al inicio: " << endl;
-    print(array, arraySize);
+    ///print(array, arraySize);
 
     cout << "Busqueda lineal -> Ingresa el elemento a buscar: ";
     cin >> searched;
-    cout << "Aqui" << endl;
     lineal_search(array, searched, arraySize);
-    cout << "Aqui" << endl;
-
+    cout << &SIZE << endl;
+    cout << &array[50] << endl;
 }
