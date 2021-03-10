1388c6c354317d5c5d7ea3704ad8a8da77f9a7cb
javirima <71288541+javirima@users.noreply.github.com>
2021-03-09 19:34:41 -0600

    Last Update Rust

--- Rust/caso3/src/main.rs
+++ Rust/caso3/src/main.rs
@@ -5,39 +5,28 @@ use std::time::Instant;
 
 fn main() {
 
-    let start = Instant::now();
-
     println!("sequencial search");
-    let mut numbers:Vec<i128> = Vec::new();
-    grow_array(&mut numbers);
-    println!("numbers:{:?}",numbers);
+    let numbers: [i32; 100000] = [0; 100000];
+   //grow_array(&mut numbers);
+   // println!("numbers:{:?}",numbers);
+    let start = Instant::now();
+    sequencial_search(&numbers,-1);
     let elapsed = start.elapsed();
-
-    sequencial_search(&mut numbers,6000);
+    println!("Tamaño de array: {}",100000);
     println!("Millis: {} ms", elapsed.as_millis());
-
-    /*
-    println!("Sort strings alphabetically");
-    let mut strings = ["beach", "hotel", "airplane", "car", "house", "art"];
-    println!("Before: {:?}", strings);
-    bubble_sort(&mut strings);
-    println!("After:  {:?}\n", strings);
-    */
 }
 
-
-pub fn grow_array(arr : &mut Vec<i128>){
+/*
+pub fn grow_array(arr : &mut ){
     let mut add_number = 5000;
-    loop{
-        if add_number == 0{
-            break;
-        }
-        arr.push(add_number);
-        add_number-=1;
+    for x in arr{
+        x
     }
 }
+*/
 
-pub fn sequencial_search<T: Ord>(arr: &mut [T], elemento_buscar:T) {
+
+pub fn sequencial_search<T: Ord>(arr: &[T], elemento_buscar:T) {
    for index in 0..arr.len() {
        if arr[index] == elemento_buscar {
            println!("Encontrado\n");
